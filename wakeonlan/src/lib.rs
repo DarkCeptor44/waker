6973fdf0932aaa4cf09bf4cd30e42391f2ab2051
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2025-08-15 12:44:26 -0300

    Initial commit

---
+++ wakeonlan/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn add(a: u64, b: u64) -> u64 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(add(2, 2), 4);
+        assert_eq!(add(0, 0), 0);
+        assert_eq!(add(1, 2), 3);
+        assert_eq!(add(100, 200), 300);
+    }
+}
