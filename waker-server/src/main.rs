bfe75c02e8f5c3ef4bafc9b82ffe3f252225b113
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2025-09-18 21:05:53 -0300

    waker-server: feat: new waker-server CLI

--- waker-server/src/main.rs
+++ waker-server/src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+#![forbid(unsafe_code)]
+#![warn(clippy::pedantic)]
+
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use colored::Colorize;
+use dotenvy::dotenv;
+use std::{path::PathBuf, process::exit};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None, propagate_version = true)]
+struct App {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    #[command(about = "Run the server")]
+    Serve {
+        #[arg(
+            short = 'H',
+            long,
+            help = "Host to listen on",
+            default_value = "0.0.0.0"
+        )]
+        host: String,
+
+        #[arg(short, long, help = "Port to listen on", default_value_t = 8080)]
+        port: u16,
+
+        #[arg(short = 'D', long, help = "Data folder to use (default: ./data)")]
+        data_folder: Option<PathBuf>,
+
+        #[arg(
+            short,
+            long,
+            help = "Number of threads to use (default: 75% of available cores)"
+        )]
+        threads: Option<usize>,
+
+        #[arg(short, long, help = "Enable debug logging", default_value_t)]
+        debug: bool,
+    },
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    if let Err(e) = run().await {
+        eprintln!("{}", format!("{e:?}").red());
+        exit(1);
+    }
+}
+
+async fn run() -> Result<()> {
+    let args = App::parse();
+
+    Ok(())
 }
