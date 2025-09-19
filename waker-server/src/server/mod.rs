cc33038be7ffc934973f96088174f9aa1d5d35e6
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2025-09-18 21:21:48 -0300

    waker-server: feat: add rest of server starting code

--- waker-server/src/server/mod.rs
+++ waker-server/src/server/mod.rs
@@ -1,10 +1,70 @@
 mod utils;
 
-use anyhow::Result;
-use std::path::PathBuf;
-use utils::is_env;
+use anyhow::{Context, Result};
+use chrono::Local;
+use log::{debug, error, LevelFilter};
+use rayon::ThreadPoolBuilder;
+use simplelog::{ColorChoice, CombinedLogger, Config, TermLogger, TerminalMode, WriteLogger};
+use std::{
+    fs::{create_dir_all, OpenOptions},
+    path::{Path, PathBuf},
+    sync::Arc,
+};
+use utils::{get_num_threads, is_env};
 
 const ENV_DEBUG: &str = "DEBUG";
+
+const FOLDER_DATA: &str = "data";
+const FOLDER_LOGS: &str = "logs";
+
+#[derive(Debug)]
+pub struct Service {
+    pub debug: bool,
+    data_folder: PathBuf,
+    logs_folder: PathBuf,
+}
+
+impl Service {
+    fn init_logger(&self) -> Result<()> {
+        let filter = if self.debug {
+            LevelFilter::Debug
+        } else {
+            LevelFilter::Info
+        };
+        let logs_folder = self.logs_folder();
+
+        create_dir_all(logs_folder).context("Failed to create logs folder")?;
+
+        let filename = Local::now().format("%Y-%m-%d").to_string();
+        let log_file_path = logs_folder.join(format!("{filename}.log"));
+        let log_file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(&log_file_path)
+            .context("Failed to open or create log file")?;
+
+        CombinedLogger::init(vec![
+            TermLogger::new(
+                filter,
+                Config::default(),
+                TerminalMode::Mixed,
+                ColorChoice::Auto,
+            ),
+            WriteLogger::new(filter, Config::default(), log_file),
+        ])
+        .context("Failed to initialize logger")?;
+
+        Ok(())
+    }
+
+    pub fn data_folder(&self) -> &Path {
+        &self.data_folder
+    }
+
+    pub fn logs_folder(&self) -> &Path {
+        &self.logs_folder
+    }
+}
 
 pub async fn start(
     host: &str,
@@ -14,6 +74,37 @@ pub async fn start(
     threads_opt: Option<usize>,
 ) -> Result<()> {
     let debug = is_env(ENV_DEBUG) || debug;
+    let data_folder = data_folder_opt.unwrap_or_else(|| PathBuf::from(FOLDER_DATA));
+    let service = Service {
+        debug,
+        data_folder: data_folder.clone(),
+        logs_folder: data_folder.join(FOLDER_LOGS),
+    };
+
+    service
+        .init_logger()
+        .context("Failed to initialize logger")?;
+
+    let num_threads = get_num_threads(threads_opt);
+    ThreadPoolBuilder::new()
+        .num_threads(num_threads)
+        .build_global()
+        .context("Failed to create thread pool")?;
+    debug!("t={num_threads}");
+
+    if let Err(e) = proceed(Arc::new(service), host, port)
+        .await
+        .context("Failed to proceed with serving backend")
+    {
+        error!("Error: {e:?}");
+        eprintln!("Error: {e:?}");
+    }
+
+    Ok(())
+}
+
+async fn proceed(service: Arc<Service>, host: &str, port: u16) -> Result<()> {
+    debug!("service={service:#?}");
 
     Ok(())
 }
--- waker-server/src/server/utils.rs
+++ waker-server/src/server/utils.rs
@@ -1,4 +1,51 @@
-use std::env::var;
+use log::warn;
+use std::{env::var, str::FromStr};
+
+pub fn get_env<S, T>(name: S, default: T) -> T
+where
+    S: AsRef<str>,
+    T: FromStr,
+{
+    let value = var(name.as_ref()).ok().and_then(|s| s.parse().ok());
+    value.unwrap_or(default)
+}
+
+#[allow(
+    clippy::cast_sign_loss,
+    clippy::cast_precision_loss,
+    clippy::cast_possible_truncation
+)]
+pub fn get_num_threads<T>(threads: T) -> usize
+where
+    T: Into<Option<usize>>,
+{
+    let cores = num_cpus::get();
+    let mut num_threads = (cores as f64 * 0.75).round() as usize;
+
+    if let Ok(env_threads_str) = var("NUM_THREADS") {
+        if let Ok(env_threads) = env_threads_str.parse::<usize>() {
+            num_threads = env_threads;
+        } else {
+            warn!("Failed to parse NUM_THREADS env var: {env_threads_str}");
+        }
+    }
+
+    if let Some(threads) = threads.into() {
+        num_threads = threads;
+    }
+
+    if num_threads == 0 {
+        num_threads = 1;
+        warn!("Number of threads was 0, using 1");
+    }
+
+    if num_threads > cores {
+        num_threads = cores;
+        warn!("Number of threads provided ({num_threads}) was greater than the number of cores ({cores}), using the number of cores");
+    }
+
+    num_threads
+}
 
 pub fn is_env<S>(name: S) -> bool
 where
