69a7c09827bfb7be2c6c42928a7a54e0984ecb2e
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2025-09-20 00:54:38 -0300

    waker-server: feat: add swagger ui and utoipa for auto api docs

--- waker-server/src/server/api/mod.rs
+++ waker-server/src/server/api/mod.rs
@@ -1,9 +1,14 @@
 mod v1;
 
-use crate::server::Service;
+use super::Service;
 use axum::Router;
 use std::sync::Arc;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+use v1::ApiDocV1;
 
 pub fn routes() -> Router<Arc<Service>> {
-    Router::new().nest("/api/v1", v1::routes())
+    Router::new()
+        .nest("/api/v1", v1::routes())
+        .merge(SwaggerUi::new("/api/v1/docs").url("/api-docs/v1/openapi.json", ApiDocV1::openapi()))
 }
--- waker-server/src/server/api/v1.rs
+++ waker-server/src/server/api/v1.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::needless_for_each)]
+
 use super::super::Service;
 use axum::{
     extract::{Path, State},
@@ -10,7 +12,34 @@ use chrono::Local;
 use log::debug;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
+use utoipa::{IntoParams, OpenApi, ToSchema};
 use waker::{create_magic_packet, wake_device, WakeOptions};
+
+#[derive(OpenApi)]
+#[openapi(
+    info(title = "Waker API v1", version = "0.1.0",),
+    paths(health, greet, wakeup),
+    components(schemas(GreetError, WakeupRequest, WakeupResponse))
+)]
+pub struct ApiDocV1;
+
+#[derive(Deserialize, ToSchema)]
+struct WakeupRequest {
+    /// MAC address of the machine to wake
+    mac: String,
+}
+
+#[derive(Serialize, ToSchema, IntoParams)]
+struct WakeupResponse {
+    /// Message
+    message: String,
+}
+
+#[derive(Serialize, ToSchema)]
+struct GreetError {
+    /// Error message
+    message: String,
+}
 
 pub fn routes() -> Router<Arc<Service>> {
     Router::new()
@@ -19,6 +48,14 @@ pub fn routes() -> Router<Arc<Service>> {
         .route("/wakeup", post(wakeup))
 }
 
+/// Health check
+#[utoipa::path(
+    get,
+    path = "/api/v1/health",
+    responses(
+        (status = 200, description = "Health check successful", body = String)
+    )
+)]
 async fn health(State(service): State<Arc<Service>>) -> String {
     let now = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
 
@@ -31,12 +68,19 @@ async fn health(State(service): State<Arc<Service>>) -> String {
 }
 
 // TODO remove this example
+/// Greet a person
+#[utoipa::path(
+    get,
+    path = "/api/v1/greet/{name}",
+    params(
+        ("name" = String, Path, description = "Name to greet", example = "John"),
+    ),
+    responses(
+        (status = 200, description = "Greeted successfully", body = String, example = "Hello, John!"),
+        (status = 400, description = "Name is empty", body = GreetError, example = json!({"message": "name cannot be empty"})),
+    )
+)]
 async fn greet(Path(name): Path<String>) -> impl IntoResponse {
-    #[derive(Serialize)]
-    struct GreetError {
-        message: String,
-    }
-
     let name = name.trim();
     if name.is_empty() {
         let status = StatusCode::BAD_REQUEST;
@@ -50,18 +94,22 @@ async fn greet(Path(name): Path<String>) -> impl IntoResponse {
     }
 }
 
-#[derive(Deserialize)]
-struct WakeupRequest {
-    mac: String,
-}
-
+/// Wake up a machine with a magic packet
+#[utoipa::path(
+    post,
+    path = "/api/v1/wakeup",
+    request_body(content = WakeupRequest, description = "WakeupRequest with MAC address", examples(
+        ("Failure" = (value = json!({"mac": "invalidmac"}))),
+        ("Success" = (value = json!({"mac": "01:23:45:67:89:AB"}))),
+    )),
+    responses(
+        (status = 200, description = "Magic packet sent", body = WakeupResponse, example = json!({"message": "Magic packet sent to 01:23:45:67:89:AB"})),
+        (status = 400, description = "WakeupRequest is invalid", body = WakeupResponse, example = json!({"message": "Invalid MAC address"})),
+        (status = 500, description = "Failed to send magic packet", body = WakeupResponse, example = json!({"message": "Failed to send magic packet"})),
+    )
+)]
 async fn wakeup(Json(payload): Json<WakeupRequest>) -> impl IntoResponse {
-    #[derive(Serialize)]
-    struct WakeupResponse {
-        message: String,
-    }
-
-    debug!("Received wakeup request for {}", payload.mac);
+    debug!("Received wakeup request for `{}`", payload.mac);
 
     let packet = match create_magic_packet(&payload.mac) {
         Ok(p) => p,
